024da7677b58efdc68600fac44cb662bee5588ef
TedWildenradt <tedkwildenradt@gmail.com>
2018-10-25 19:26:59 -0700

    Make Map class

--- W1D5/HW_DIY_ADT.rb
+++ W1D5/HW_DIY_ADT.rb
@@ -19,3 +19,54 @@ class Stack
     @stack.last
   end
 end
+
+
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue.push(el)
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def peek
+    @queue.first
+  end
+end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def set(key, value)
+    self.each do |array|
+      return if array[0] == key
+    end
+    self.push([key,value])
+  end
+
+  def get(key)
+    self.each do |array|
+      if array[0] == key
+        return array[1]
+      end
+    end
+    nil
+  end
+
+  def delete(key)
+    i = nil
+    self.each_with_index do |array, idx|
+      if array[0] == key
+        i = idx
+      end
+    end
+    self.delete_at(i) unless i.nil?
+  end
+end
