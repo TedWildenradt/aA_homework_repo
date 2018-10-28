5c9a16fc751a5a6baadf392cb734e380593d3f1a
TedWildenradt <tedkwildenradt@gmail.com>
2018-10-28 14:28:17 -0700

    Add #reset_game, finish #show_sequence and #require_sequence

--- W2D1/skeleton/lib/simon.rb
+++ W2D1/skeleton/lib/simon.rb
@@ -12,6 +12,7 @@ class Simon
   def play
     until game_over
       take_turn
+      # system 'clear'
     end
     game_over_message
     reset_game
@@ -20,34 +21,55 @@ class Simon
   def take_turn
     show_sequence
     require_sequence
+
+    unless @game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
+    @seq.each do |color|
+      puts color
+      sleep 1
+      system 'clear'
+    end
   end
 
   def require_sequence
+    i = 0
+    until i == @sequence_length || @game_over == true
+      puts "Enter color"
+      input = gets.chomp
+      if input != seq[i]
+        @game_over = true
+      end
+      i += 1
+    end
 
   end
 
   def add_random_color
-    seq.push(COLORS.sample)
+    @seq.push(COLORS.sample)
   end
 
   def round_success_message
-
+    puts "Success! On to the next round"
   end
 
   def game_over_message
-
+    puts "Game over"
   end
 
   def reset_game
-    sequence_length = 1
-    game_over = false
-    seq = []
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
+  simon = Simon.new
+  simon.play
 end
