b8a34eb52a62f7adc6e36738dce5e4c76fa00742
TedWildenradt <tedkwildenradt@gmail.com>
2018-10-31 07:51:41 -0700

    Finish #sluggish

--- W2D4/octopus.rb
+++ W2D4/octopus.rb
@@ -1,6 +1,17 @@
 # Sluggish Octopus
 def sluggish(array)
+  len = array.length
 
+  (0...len).reverse_each do |i|
+    (0...len - 1).each do |j|
+      if array[j] > array[j+1]
+        temp = array[j]
+        array[j] = array[j+1]
+        array[j+1] = temp
+      end
+    end
+  end
+  array.last
 end
 
 def dominant(array)
