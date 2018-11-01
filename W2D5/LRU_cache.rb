c5ba96debefb2220b0a87439936b92d09e6dd19a
TedWildenradt <tedkwildenradt@gmail.com>
2018-11-01 12:56:52 -0700

    Finish #add method

--- W2D5/LRU_cache.rb
+++ W2D5/LRU_cache.rb
@@ -9,24 +9,32 @@ class LRUCache
   end
 
   def add(el)
-    raise SizeError unless count <
+    if self.count == @max_length && @cache.include?(el)
+      @cache.delete(el)
+      @cache.push(el)
+    elsif self.count == @max_length
+      @cache.shift
+      @cache.push(el)
+    else
+      @cache.push(el)
+    end
   end
 
   def show
     print @cache
   end
 
-  private
-  def push(el)
-    @cache.push
-  end
-
-  def shift
-    @cache.shift
-  end
-
-  def check(el)
-    @cache.index(el)
-  end
+  # private
+  # def push(el)
+  #   @cache.push(el)
+  # end
+  #
+  # def shift
+  #   @cache.shift
+  # end
+  #
+  # def check(el)
+  #   @cache.index(el)
+  # end
 
 end
