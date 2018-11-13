409c4cc7d0d7e553b61c00c268e32df35c8b67e4
TedWildenradt <tedkwildenradt@gmail.com>
2018-11-13 08:32:32 -0800

    Add session token methods to user

--- W4D3/app_name/app/models/user.rb
+++ W4D3/app_name/app/models/user.rb
@@ -5,8 +5,48 @@ class User < ApplicationRecord
   validates :session_token, presence: true
   after_initialize :ensure_session_token
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
+
+  def self.find_by_credentials
+
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
