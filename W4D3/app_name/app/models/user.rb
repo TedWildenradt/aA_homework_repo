694430c5150c8c8d0201094fe0deb8662a8f5518
TedWildenradt <tedkwildenradt@gmail.com>
2018-11-13 08:26:17 -0800

    Add validations to user model

--- W4D3/app_name/app/models/user.rb
+++ W4D3/app_name/app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
-  validates :username, :session_token, presence: true
+  validates :username, presence: true
   validates :password_digest, presence: { message: 'Password can\'t be blank' }
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :session_token, presence: true
+  after_initialize :ensure_session_token
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end
